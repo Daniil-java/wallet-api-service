612f9e12fb3964d810e15bc2d8985d902991f371
ПК <saponembigas@gmail.com>
2024-12-06 16:43:54 +0700

    init

---
+++ src/main/java/com/review/wallet_api_service/WalletApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.review.wallet_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/review/wallet_api_service/WalletApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.wallet_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
